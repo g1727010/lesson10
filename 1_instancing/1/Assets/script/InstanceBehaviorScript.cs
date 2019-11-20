d8b3f811889a8a92b16e3055913918731a584085
1727010 <katsuma4869@icloud.com>
2019-11-20 15:53:29 +0900

    仮

---
+++ 1_instancing/1/Assets/script/InstanceBehaviorScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class InstanceBehaviorScript : MonoBehaviour {
+
+    public Color color = Color.white;
+	void Start () {
+        var renderer = GetComponent<Renderer>();
+        Assert.IsNotNull(renderer);
+
+        MaterialPropertyBlock block = new MaterialPropertyBlock();
+        block.SetColor("_Color", color);
+
+        renderer.SetPropertyBlock(block);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
